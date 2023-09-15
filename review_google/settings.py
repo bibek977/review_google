8d561dbb0cdc3ff0d397aebc9c51aa746fd2bb36
bibek977 <bhattaraibibek91@gmail.com>
2023-09-15 18:07:56 +0545

    json renderer corsheader and host to connect react

--- review_google/settings.py
+++ review_google/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
 EXTERNAL_APPS = [
     'core',
     'rest_framework',
+    'corsheaders',
 ]  
 INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +139,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+	"http://localhost:3000",
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
